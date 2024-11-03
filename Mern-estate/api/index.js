912c50dac1c502ece87e3e4f22bf9ae603a92893
Yaksha-sn <snyakshaa@gmail.com>
2024-11-03 19:01:11 +0530

    create user model

--- Mern-estate/api/index.js
+++ Mern-estate/api/index.js
@@ -19,3 +19,8 @@ const app= express();
 app.listen(3000,()=>{
     console.log('Server is runing on port 3000!!!');
 });
+
+
+app.get('/test', (req, res)=>{
+    res.json({message: 'API is working!' });
+});
